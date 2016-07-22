83eb7692bbe2bed2affee311e7db46a143b66050
Poonam Deshpande <pvd005@gmail.com>
2016-07-22 10:02:26 +0530

    Code Check

--- main.cpp
+++ main.cpp
@@ -14,12 +14,12 @@
 extern class Pipeline Pipeline_obj;
 extern class Report   Report_obj;
 
-int main(int argc, const char * argv[])
+int  main(int argc, const char * argv[])
 {
     uint8_t i;
 
     Report_obj.write_header_to_report();
-    for (i  = 0; i < 10; i++)
+    for (i  = 0; i < 20; i++)
     {
         Pipeline_obj.run_pipeline (i);
     }
